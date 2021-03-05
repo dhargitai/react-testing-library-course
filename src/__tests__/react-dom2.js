1d2349c72b21c444ac88a086d233d833efb83b28
David Hargitai <david@leadforward.hu>
2021-03-05 15:45:22 +0100

    favorite number 3 - use screen, render and getByLabelText from tl/react

--- src/__tests__/react-dom2.js
+++ src/__tests__/react-dom2.js
@@ -1,11 +1,10 @@
 import * as React from 'react'
-import ReactDOM from 'react-dom'
 import '@testing-library/jest-dom/extend-expect'
+import {screen, render} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
 test('renders a number input with the label "Favorite Number"', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<FavoriteNumber />, div)
-  expect(div.querySelector('input')).toHaveAttribute('type', 'number')
-  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
+  render(<FavoriteNumber />)
+  const input = screen.getByLabelText('Favorite Number')
+  expect(input).toHaveAttribute('type', 'number')
 })
