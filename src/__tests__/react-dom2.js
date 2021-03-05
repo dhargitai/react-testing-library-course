ebf8ae1ed74ba95adcb2b1a909be8e871a9417d1
David Hargitai <david@leadforward.hu>
2021-03-05 15:56:09 +0100

    favorite number 5 - use debug

--- src/__tests__/react-dom2.js
+++ src/__tests__/react-dom2.js
@@ -4,7 +4,14 @@ import {screen, render} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
 test('renders a number input with the label "Favorite Number"', () => {
-  render(<FavoriteNumber />)
+  const {debug} = render(<FavoriteNumber />)
   const input = screen.getByLabelText(/favorite number/i)
   expect(input).toHaveAttribute('type', 'number')
+  debug(input)
 })
+
+// disabling the rule for the purposes of the exercise
+/*
+eslint
+  testing-library/no-debug: "off",
+*/
