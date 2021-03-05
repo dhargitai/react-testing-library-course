84ff7749727f608f9102a234cad852f4e2b52b60
David Hargitai <david@leadforward.hu>
2021-03-05 15:33:40 +0100

    favorite number 2

--- src/__tests__/react-dom2.js
+++ src/__tests__/react-dom2.js
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
+import '@testing-library/jest-dom/extend-expect'
 import {FavoriteNumber} from '../favorite-number'
 
-test('renders a number input with the labe "Favorite Number"', () => {
+test('renders a number input with the label "Favorite Number"', () => {
   const div = document.createElement('div')
   ReactDOM.render(<FavoriteNumber />, div)
-  expect(div.querySelector('input').type).toBe('number')
+  expect(div.querySelector('input')).toHaveAttribute('type', 'number')
   expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
 })
